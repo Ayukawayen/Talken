9c303162cac59867991046fa958999e58c48e10e
Ayukawayen <Ayukawayen@Gmail.com>
2021-05-18 16:42:05 +0800

    feat: 增加點擊控制

--- js/main_comp.js
+++ js/main_comp.js
@@ -5,26 +5,54 @@ Vue.component('v_main', {
 		<main>
 			<div class="panel">
 				<textarea id="editor" placeholder="What's new"></textarea>
-				<button class="post" v-if="account.genesiser.gpBalance>0" @click="onPostClick">Post [{{ account.genesiser.gpBalance }}]</button>
-				<button class="post" v-if="account.genesiser.gpBalance<=0" @click="onRequestGpClick">Get some TalkenG to post</button>
-				<button class="post" v-if="account.genesiser.gpBalance=='Loading'" disabled="disabled">…</button>
+				<button class="post" @click="onPostClick" :disabled="isPostDisabled">{{ account.genesiser.gpBalance | postButtonText }}</button>
 			</div>
 		
 			<v_talken_list :items="talkens"></v_talken_list>
 		</main>
 	`,
+	data:()=>({
+		isPending: false,
+	}),
 	props: ['account', 'talkens'],
+	computed: {
+		isPostDisabled: function() {
+			return this.isPending || this.account.genesiser.gpBalance=='Loading';
+		},
+	},
+	filters: {
+		postButtonText: function(value) {
+			if(value == 'Loading') return '…';
+			if(value > 0) return `Post [${value}]`;
+			if(value <= 0) return 'Get some TalkenG to post';
+			
+			return '';
+		},
+	},
 	methods: {
 		onPostClick: function() {
-			let text = document.querySelector('#editor').value;
-			postTalken(text).then(()=>{
-				loadTalkens().then(()=>{});
-			});
-		},
-		onRequestGpClick: function() {
-			requestGp().then(()=>{
-				loadBalance();
-			});
+			if(this.account.genesiser.gpBalance=='Loading') return;
+			
+			this.isPending = true;
+			
+			if(this.account.genesiser.gpBalance > 0) {
+				let text = document.querySelector('#editor').value;
+				postTalken(text).then(()=>{
+					this.isPending = false;
+					loadTalkens().then(()=>{});
+				});
+				return;
+			}
+			
+			if(this.account.genesiser.gpBalance <= 0) {
+				requestGp().then(()=>{
+					this.isPending = false;
+					loadBalance();
+				});
+				return;
+			}
+			
+			this.isPending = false;
 		},
 	},
 });
